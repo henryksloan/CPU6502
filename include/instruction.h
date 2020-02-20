a358b65cb858b07d69ba3169b887788728302d96
Henry Sloan <henryksloan@gmail.com>
2020-02-19 20:01:48 -0500

    Outline refactored instruction implementation

---
+++ include/instruction.h
@@ -0,0 +1,37 @@
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include <string>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+typedef struct AddrMode {
+    const std::string format;
+    short length;
+} AddrMode;
+
+typedef struct InstrMode {
+    uint8_t opcode;
+    std::string mode_str;
+    int cycles;
+} InstrMode;
+
+typedef struct InstrInfo {
+    std::string op_str;
+    std::string mode_str;
+    int cycles;
+} InstrInfo;
+
+class Instructions {
+ public:
+    using instr_map_t = std::unordered_map<uint8_t, InstrInfo>;
+    static const instr_map_t instr_map;
+    static const std::unordered_map<std::string, AddrMode> mode_map;
+
+ private:
+    static void add_instr(instr_map_t &map, std::string op_str, std::vector<InstrMode> modes);
+    static instr_map_t generate_instr_map();
+};
+
+#endif // INSTRUCTION_H
